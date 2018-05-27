38f1519680c557c78d0787348dc399383628d9c9
Nik Vangerow <n@vangerow.uk>
2018-05-27 18:29:57 +0100

    early scraper stuff. Currently gets all but place data.
    Not yet integrated with the rest of the codebase (does not yet work)

--- src/fb.js
+++ src/fb.js
@@ -1,26 +1,36 @@
 "use strict";
 
-const graph    = require('fbgraph'),
-      jsonfile = require('jsonfile')
-
-// Set the access token
-graph.setAccessToken(jsonfile.readFileSync("config.json").access_token)
-
-async function facebookFetch(url) {
-  return new Promise((resolve, reject) => {
-    graph.get(url, (err, res) => {
-      if (err !== null) {
-        reject(JSON.stringify(err))
-        return
-      }
-
-      resolve(res)
-    })
-  })
-}
+const rqrs  = require('request-promise'),
+      cheerio = require('cheerio')
 
 module.exports = async id => {
-  const url =
-    `${id}?fields=cover,description,start_time,end_time,event_times,name,place,timezone`
-  return await facebookFetch(url)
+  // const url =
+  //   `${id}?fields=cover,description,start_time,end_time,event_times,name,place,timezone`
+  // return await facebookFetch(url)
+  const requested = await rqrs({
+    url: `https://www.facebook.com/events/${id}/`,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1 Safari/605.1.15'
+    }
+  })
+  const $ = cheerio.load(requested.replace(/<code.*?><!--(.*)--><\/code>/g, "$1"))
+
+  const dateTimeRegex = /(.*?)\sto\s.(.*?)/g
+  const fetchedDateTime = $("._publicProdFeedInfo__timeRowTitle").attr("content")
+
+  const $place = $("#event_summary ul > li:nth-of-type(2) table > tbody > tr > td:nth-of-type(2) > div > div > div > div:nth-of-type(2) > div").html()
+
+  return {
+    cover: {
+      source: $("#event_header_primary > div > a[ajaxify][data-ploi]").attr("data-ploi")
+    },
+    description: $("span[data-testid=\"event-permalink-details\"]").html(),
+    start_time: fetchedDateTime.replace(dateTimeRegex, "$1"),
+    end_time: fetchedDateTime.replace(dateTimeRegex, "$1"),
+    name: $("h1[data-testid=\"event-permalink-event-name\"]").html(),
+    place: {
+      name: $place.find("span").html(),
+      location: $place.find("div").html()
+    }
+  }
 }
